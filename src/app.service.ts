f23b5c0f5913b04292fce4b335778513e096e146
Araden <2713886816@qq.com>
2023-07-27 14:04:09 +0800

    fix: 登陆时用户信息解析方式

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,12 @@ import { PrismaClient } from '@prisma/client';
 import { LoginDto } from './dto/login.dto';
 import { config as configEnv } from 'dotenv';
 
-// 格式 USERINFO=username:xxx#password:yyy
+// 格式 USERINFO=username:xxx~password:yyy，用井号分割后面的会被当成注释/(ㄒoㄒ)/~~
 const { parsed } = configEnv({
   path: '.env.local',
 });
 const userInfo: LoginDto | Record<string, never> = Object.fromEntries(
-  parsed.USERINFO?.split('#').map((pairs) => pairs.split(':')) || [],
+  parsed.USERINFO?.split('~').map((pairs) => pairs.split(':')) || [],
 );
 
 @Injectable()
